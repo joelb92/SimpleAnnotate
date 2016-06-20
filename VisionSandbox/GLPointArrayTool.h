d073a6f0998ffa1c64ed4d90861d3822a56d31da
Brogan, Joel R <broganjr@ornl.gov>
2016-06-20 17:21:48 -0400

    Refactor of main data structure functions in GLViewTools

---
+++ VisionSandbox/GLPointArrayTool.h
@@ -0,0 +1,13 @@
+//
+//  GLPointArrayTool.h
+//  SimpleAnnotate
+//
+//  Created by Brogan, Joel R. on 6/20/16.
+//  Copyright © 2016 University of Notre Dame. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GLPointArrayTool : NSObject
+
+@end
---
+++ VisionSandbox/GLPointArrayTool.mm
@@ -0,0 +1,13 @@
+//
+//  GLPointArrayTool.m
+//  SimpleAnnotate
+//
+//  Created by Brogan, Joel R. on 6/20/16.
+//  Copyright © 2016 University of Notre Dame. All rights reserved.
+//
+
+#import "GLPointArrayTool.h"
+
+@implementation GLPointArrayTool
+
+@end
