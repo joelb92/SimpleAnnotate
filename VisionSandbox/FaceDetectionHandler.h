437dbc8887c88b2b33665267090b1709b697a23f
Brogan, Joel R <broganjr@ornl.gov>
2016-06-23 16:54:04 -0400

    Added face detection functionality

---
+++ VisionSandbox/FaceDetectionHandler.h
@@ -0,0 +1,13 @@
+//
+//  FaceDetectionHandler.h
+//  SimpleAnnotate
+//
+//  Created by Brogan, Joel R. on 6/23/16.
+//  Copyright © 2016 University of Notre Dame. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FaceDetectionHandler : NSObject
+
+@end
---
+++ VisionSandbox/FaceDetectionHandler.mm
@@ -0,0 +1,13 @@
+//
+//  FaceDetectionHandler.m
+//  SimpleAnnotate
+//
+//  Created by Brogan, Joel R. on 6/23/16.
+//  Copyright © 2016 University of Notre Dame. All rights reserved.
+//
+
+#import "FaceDetectionHandler.h"
+
+@implementation FaceDetectionHandler
+
+@end
